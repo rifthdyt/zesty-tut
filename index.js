4df35813c672b24876ddcbb2f281d7905610ca90
rifthdyt <37908851+rifthdyt@users.noreply.github.com>
2018-05-06 00:03:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,19 @@ bot.on("message", async message => {
   if (cmd === `${prefix}ping`){
     message.channel.send("Pong!");
   }
+  bot.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}help`){
+    message.channel.send("nah!");
+  }
 
 });
 
