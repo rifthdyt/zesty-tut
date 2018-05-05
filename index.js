94bdde6032292a41ec3dd92bd0533377c23ec48e
rifthdyt <37908851+rifthdyt@users.noreply.github.com>
2018-05-05 23:32:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ bot.on("message", async message => {
 
   if (cmd === `${prefix}ping`){
     message.channel.send("Pong!");
+    
+  if (command === "foo"){
+    mesage.channel.sendMessage("bar!");
   }
+
 });
 
 bot.login(process.env.token);
