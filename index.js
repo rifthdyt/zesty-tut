0abaffa366a3e4da59ea2fdc15133f18d5db9bac
rifthdyt <37908851+rifthdyt@users.noreply.github.com>
2018-05-06 11:43:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,19 @@ bot.on("message", async message => {
   if (cmd === `${prefix}youtube`){
     message.channel.send("https://www.youtube.com/channel/UC00U2crvBmhXNu9pmQRhH3g");
   }
+  bot.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}ich`){
+    message.channel.send("https://www.youtube.com/channel/UC00U2crvBmhXNu9pmQRhH3g");
+  }
 
 
 });
