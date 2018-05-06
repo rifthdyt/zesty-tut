ab2d41976f214f15ee59d13bb3ee653214573d17
rifthdyt <37908851+rifthdyt@users.noreply.github.com>
2018-05-06 17:06:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -58,6 +58,50 @@ bot.on("message",function(message) {
             message.channel.send("https://twitter.com/World_inWar?s=9");
         }
     });
+    
+    bot.on("message", async message => {
 
+        if (message.author.bot) return;
+        if (message.channel.type === "dm") return;
+
+        let prefix = '$';
+        let messageArray = message.content.split(" ");
+        let cmd = messageArray[0];
+        let args = messageArray.slice(1);
+
+        if (cmd === `${prefix}team`) {
+            message.channel.send("RIFT HD: BOSS DEVELOPER AND MAIN OWNER OF THIS GAME; LEON: BUG FIXER AND MODELING DEVELOPER; MR.ROUGHZ CODER AND BUG FIXER; MIKEX: COMMUNITY MANAGER AND MOD");
+        }
+    });
+
+    bot.on("message", async message => {
+
+        if (message.author.bot) return;
+        if (message.channel.type === "dm") return;
+
+        let prefix = '$';
+        let messageArray = message.content.split(" ");
+        let cmd = messageArray[0];
+        let args = messageArray.slice(1);
+
+        if (cmd === `${prefix}instagram`) {
+            message.channel.send("https://www.instagram.com/worldinwargame/");
+        }
+    });
+
+    bot.on("message", async message => {
+
+        if (message.author.bot) return;
+        if (message.channel.type === "dm") return;
+
+        let prefix = '$';
+        let messageArray = message.content.split(" ");
+        let cmd = messageArray[0];
+        let args = messageArray.slice(1);
+
+        if (cmd === `${prefix}invite`) {
+            message.channel.send("https://discordapp.com/invite/GNEz66a");
+        }
+    });
 
 bot.login(process.env.token);
