73c84b7caff1f9888a6add55ebe762ce9f57ae36
rifthdyt <37908851+rifthdyt@users.noreply.github.com>
2018-05-06 16:02:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,21 @@ bot.on("message", async message => {
     message.channel.send("https://www.youtube.com/channel/UC00U2crvBmhXNu9pmQRhH3g");
   }
 
+ 
+bot.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '+';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}inis`){
+    message.channel.send("hii");
+  }
+
 
 });
 
