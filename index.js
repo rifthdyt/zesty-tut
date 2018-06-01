c9133a9fabeacb1a2858c1b20d2a50773b007b4a
rifthdyt <37908851+rifthdyt@users.noreply.github.com>
2018-06-01 13:16:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -119,4 +119,19 @@ bot.on("message",function(message) {
         }
     });
 
+
+bot.on("message", async message => {
+
+        if (message.author.bot) return;
+        if (message.channel.type === "dm") return;
+
+        let prefix = '$';
+        let messageArray = message.content.split(" ");
+        let cmd = messageArray[0];
+        let args = messageArray.slice(1);
+
+        if (cmd === `${prefix}donate`) {
+            message.channel.send("https://www.patreon.com/worldinwar");
+        }
+    });
 bot.login(process.env.token);
