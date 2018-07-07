3c0303c0294775f4c2672a68e7076c5a0f596465
rifthdyt <37908851+rifthdyt@users.noreply.github.com>
2018-07-07 21:39:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,60 @@ bot.on("ready", async () => {
   bot.user.setActivity(`WORLD IN WAR`);
 });
 
+bot.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'welcome');
+    let memberavatar = member.user.avatarURL
+    if (!channel) return;
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':bust_in_silhouette: | name : ', `${member}`)
+        .addField(':microphone2: | You joined our server so we hope u have much fun:)!', `WELCOME TO OUR SERVER, ${member}`)
+        .addField(':id: | User :', "**[" + `${member.id}` + "]**")
+        .addField(':family_mwgb: | You are now a member!', `${member.guild.memberCount}`)
+        .addField("Name", `<@` + `${member.id}` + `>`, true)
+        .addField('Server', `${member.guild.name}`, true)
+        .setFooter(`**${member.guild.name}**`)
+        .setTimestamp()
+
+    channel.sendEmbed(embed);
+});
+bot.on('guildMemberAdd', member => {
+
+    console.log(`${member}`, "has joined" + `${member.guild.name}`)
+
+});
+
+
+bot.on('guildMemberRemove', member => {
+    let channel = member.guild.channels.find('name', 'welcome');
+    let memberavatar = member.user.avatarURL
+    if (!channel) return;
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField('Name:', `${member}`)
+        .addField('Has left the server', ';(')
+        .addField('BYE BYE :(. :(', 'WE HOPE U WILL JOIN OUR SERVER AGAIN')
+        .addField('Members', `${member.guild.memberCount}` + " Members")
+        .setFooter(`**${member.guild.name}`)
+        .setTimestamp()
+
+    channel.sendEmbed(embed);
+});
+
+bot.on('guildMemberRemove', member => {
+    console.log(`${member}` + "has left" + `${member.guild.name}` + "Sending leave message now")
+    console.log("Leave Message Sent")
+});
 
 bot.on("message",function(message) {
     if (message.content == "hello") {
+        message.channel.sendMessage("HI, THERE");
+    }
+    });
+ bot.on("message",function(message) {
+    if (message.content == "hi") {
         message.channel.sendMessage("HI, THERE");
     }
     });
@@ -58,21 +109,29 @@ bot.on("message",function(message) {
             message.channel.send("https://twitter.com/World_inWar?s=9");
         }
     });
-    
-    bot.on("message", async message => {
 
-        if (message.author.bot) return;
-        if (message.channel.type === "dm") return;
+bot.on("message", async message => {
 
-        let prefix = '$';
-        let messageArray = message.content.split(" ");
-        let cmd = messageArray[0];
-        let args = messageArray.slice(1);
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
 
-        if (cmd === `${prefix}team`) {
-            message.channel.send("RIFT HD: BOSS DEVELOPER AND MAIN OWNER OF THIS GAME; LEON: BUG FIXER AND MODELING DEVELOPER; MR.ROUGHZ CODER AND BUG FIXER; MIKEX: COMMUNITY MANAGER AND MOD");
-        }
-    });
+    let prefix = '$';
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if (cmd === `${prefix}team`) {
+    var emebed = new Discord.RichEmbed()
+    .addField("OUR DEVELOPER TEAM")
+    .addField("▶ BOSS DEVELOPER:", "RIFT")
+    .addField("▶ CEO:", "MrROUGHZ")
+    .addField("▶ BUG FIXER etc.:", "LEON K")
+    .addField("▶ SKIN+MAP DESIGNER:", "kaguya")
+    .setColor(0xff0000)
+   message.channel.sendEmbed(emebed);
+    }
+
+});
 
     bot.on("message", async message => {
 
